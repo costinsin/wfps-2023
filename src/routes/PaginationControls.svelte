501b2d0f74a2f28fa66039a61c6f567b2d2c8653
Jan Kühle <jankuehle@google.com>
2023-07-19 15:01:47 +0000

    Style pagination button

--- src/routes/PaginationControls.svelte
+++ src/routes/PaginationControls.svelte
@@ -3,5 +3,18 @@
 </script>
 
 <div class="pagination">
-	<a href="?next={next}" hidden={next == null}>Next</a>
+	<a href="?next={next}" hidden={next == null}>Next page</a>
 </div>
+
+<style>
+	.pagination {
+		margin-top: 2em;
+	}
+
+	.pagination a {
+		display: inline-block;
+		border: 1px solid var(--color-theme-1);
+		border-radius: 0.2em;
+		padding: 0.5em 1em;
+	}
+</style>
