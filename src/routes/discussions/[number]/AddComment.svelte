ec6eeb1f65ce9e76bf293df34eb19a0aba309a94
Pawel Kozlowski <pkozlowski@google.com>
2023-07-20 14:07:42 +0200

    feat: add new comment form

---
+++ src/routes/discussions/[number]/AddComment.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	let newCommentText = '';
+</script>
+
+<form method="POST" action="?/addComment">
+	<textarea
+		name="commentText"
+		bind:value={newCommentText}
+		placeholder="Write a comment"
+		cols="100"
+		rows="5"
+	/>
+	<br />
+	<button type="submit" disabled={newCommentText.length === 0}>Comment</button>
+</form>
