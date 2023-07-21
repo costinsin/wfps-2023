9bab6745aefff1de629cc54eda67143dff84fe7e
Pawel Kozlowski <pkozlowski@google.com>
2023-07-21 11:38:19 +0200

    feat: reactions everywhere

---
+++ src/routes/discussions/[number]/Reactions.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	import { REACTION_EMOJI } from '$lib/reactions';
+	import type { ReactionGroup } from '$lib/server/github';
+	import AddReaction from './AddReaction.svelte';
+
+	export let reactions: ReactionGroup[];
+	export let objectId: string;
+</script>
+
+<div class="reactions">
+	{#each reactions as group (group.content)}
+		<button disabled>
+			{REACTION_EMOJI[group.content]}
+			{group.totalCount}
+		</button>{' '}
+	{/each}
+	<AddReaction {objectId} />
+</div>
